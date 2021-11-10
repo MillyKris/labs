ff44e5a3ba52a4292decdcd367420952b5ccf058
MillyKris <89028750+MillyKris@users.noreply.github.com>
2021-11-10 22:37:01 +0400

    Update userActions.php

--- LR3/userActions.php
+++ LR3/userActions.php
@@ -1,5 +1,4 @@
 <?php
-//session_start();
 require_once ($_SERVER['DOCUMENT_ROOT'] . '/LR3/diffunctions.php');
 
 class userActions
@@ -9,6 +8,10 @@ class userActions
 			return "";
 		}
 		$message = UserLogic::signIn($_POST['email'], $_POST['password']);
+		if(!strlen($message)){
+			header(header: 'Location:web-applications.php');
+			die();
+		}
 		return $message;
 	}
 	public static function signUp() :string{
@@ -16,10 +19,10 @@ class userActions
 			return "";
 		}
 		$message = UserLogic::signUp($_POST['email'], $_POST['fullName'], $_POST['bloodtype'], $_POST['factor'], $_POST['vk'], $_POST['password'], $_POST['dateOfBirth'], $_POST['address'], $_POST['gender'], $_POST['interests'], $_POST['password2']);
-		/*if(!strlen($message)){
-			header('Location: ' . $_SERVER['PHP_SELF'] . '?success=y');
+		if(!strlen($message)){
+			header(header: 'Location:web-applications.php');
 			die();
-		}*/
+		}
 		return $message;
 	}
 }
