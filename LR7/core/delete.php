4838990fe9bc2500964aa1e5d0d5da03a0b0f0af
MillyKris <89028750+MillyKris@users.noreply.github.com>
2021-12-22 14:26:27 +0400

    Update delete.php

--- LR7/core/delete.php
+++ LR7/core/delete.php
@@ -2,13 +2,10 @@
  require_once ($_SERVER['DOCUMENT_ROOT'] . '/L7/.core/connect.php');
  require_once ($_SERVER['DOCUMENT_ROOT'] . '/L7/.core/tables.php');
  require_once ($_SERVER['DOCUMENT_ROOT'] . '/L7/.core/logic.php');
-echo "QQQQ";
 if('GET' == $_SERVER['REQUEST_METHOD'] && isset($_GET['data-id-item'])){
 	$id = $_GET['data-id-item'];
-	echo $id;
 	$ar = userLogic::getImage($id);
 	$filepath = $_SERVER['DOCUMENT_ROOT'] . '/L7/'.$ar[0]["img_path"];
-	echo $filepath;
 	unlink($filepath);
 	$errors = userLogic::delete($id);
 	if(strlen($errors) > 0){
