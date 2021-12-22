3b6dbf95996cd1e24696d8c4ac34f37aeffed3d8
MillyKris <89028750+MillyKris@users.noreply.github.com>
2021-12-22 14:24:11 +0400

    Delete update.php

--- LR7/core/update.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/L7/.core/tables.php'); 
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/L7/.core/logic.php'); 
-
-var_dump($_GET);
-$id = $_GET['data-id-item'];
-$var = userLogic::getById($id);
-var_dump($var);
